25516762a4093ed9e88db17a0a47d99ac6043926
Konnor88 <39319697+Konnor88@users.noreply.github.com>
2024-03-13 21:24:02 -0400

    Update background.js

--- firefox/background.js
+++ firefox/background.js
@@ -1,12 +1,64 @@
-browser.webRequest.onBeforeRequest.addListener(function(details) { 
-	browser.tabs.update({
- url: "blocked.html"
-});
-	return {cancel: true};
+browser.webRequest.onBeforeRequest.addListener(
+    function(details) { 
+        browser.tabs.update({
+            url: "blocked.html"
+        });
+        return {cancel: true};
+    },
+    {urls: [
+        "*://grabify.link/*",
+        "*://mymassive.yachts/*",
+        "*://stonks.boats/*",
+        "*://stonks.fun/*",
+        "*://toes.beauty/*",
+        "*://barefoot.pics/*",
+        "*://shareit.pics/*",
+        "*://gamer.tattoo/*",
+        "*://shipment.website/*",
+        "*://imagevault.cloud/*",
+        "*://sugma.mom/*",
+        "*://yum.mom/*",
+        "*://plz.life/*",
+        "*://massive.mom/*",
+        "*://massive.boats/*",
+        "*://mymassive.store/*",
+        "*://mymassive.top/*",
+        "*://gamer.hair/*",
+        "*://screenshare.pics/*",
+        "*://myprivate.pics/*",
+        "*://noodshare.pics/*",
+        "*://stopify.co/*",
+        "*://grabb.site/*",
+        "*://grabify.icu/*",
+        "*://grabify.world/*",
+        "*://grabifyicu.com/*",
+        "*://iplogger.org/*",
+        "*://2no.co/*",
+        "*://iplogger.com/*",
+        "*://iplogger.ru/*",
+        "*://yip.su/*",
+        "*://iplist.ru/*",
+        "*://02ip.ru/*",
+        "*://iplogger.co/*",
+        "*://iplogger.info/*",
+        "*://ipgraber.ru/*",
+        "*://ipgrabber.ru/*",
+        "*://iplist.ru/*",
+        "*://ezstat.ru/*",
+        "*://iplis.ru/*",
+        "*://maper.info/*",
+        "*://wl.gl/*",
+        "*://ed.tc/*",
+        "*://bc.ax/*",
+        "*://ps3cfw.com/*",
+        "*://tracker.iplocation.net/*",
+        "*://yȯutube.com/*",
+        "*://gyazo.nl/*",
+        "*://goo.by/*"
+    ]},
+    ["blocking"]
+);
 
-},
-{urls: ["*://grabify.link/*", "*://stopify.co/*", "*://fortnight.space/*", "*://fortnitechat.site/*","*://joinmy.site/*", "*://iplogger.org/*", "*://2no.co/*", "*://iplogger.com/*", "*://iplogger.ru/*", "*://yip.su/*", "*://catsnthing.com/*", "*://catsnthings.fun/*", "*://www.ps3cfw.com/*", "*://iplis.org/*","*://02ip.ru/*", "*://iplogger.co/*","*://iplogger.info/*", "*://ipgraber.ru/*", "*://lovebird.guru/*", "*://trulove.guru/*", "*://dateing.club/*", "*://shrekis.life/*",  "*://headshot.monster/*", "*://gaming-at-my.best/*", "*://progaming.monster/*",  "*://gamingfun.me/*", "*://ipgrabber.ru/*", "*://iplist.ru/*", "*://ezstat.ru/*", "*://yourmy.monster/*", "*://screenshot.best/*", "*://grabify.world/*","*://grabify.icu/*", "*://iplis.ru/*", "*://partpicker.shop/*","*://sportshub.bar/*","*://locations.quest/*","*://screenshare.pics/*", "*://noodshare.pics/*", "*://cheapcinema.club/*", "*://shhh.lol/*", "*://maper.info/*","*://tracker.iplocation.net/*","*://grabifyicu.com/*","*://yȯutube.com/*","*://gyazo.nl/*","*://myprivate.pics/*","*://toldyouso.pics/*","*://toldyouso.lol/*","*://imagehost.pics/*","*://imghost.pics/*","*://gamertag.shop/*","*://pichost.pics/*","*://picshost.pics/*","*://gamergirl.pro/*","*://thisdomainislong.lol/*","*://foot.wiki/*","*://photovault.pics/*","*://iplis.ru/*","*://wl.gl/*","*://ed.tc/*","*://bc.ax/*" ]},
-["blocking"]);
 browser.browserAction.setTitle({
     title:'Anti-Grabify is active.'
 });
